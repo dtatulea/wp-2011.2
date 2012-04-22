f90bfc0de2abd963dcb40f531b7a33ae6c7650f8
Dragos Tatulea <dragos.tatulea@gmail.com>
2012-04-22 18:22:10 +0300

    rm cathegory print

--- category.php
+++ category.php
@@ -15,9 +15,6 @@ get_header(); ?>
 			<?php if ( have_posts() ) : ?>
 
 				<header class="page-header">
-					<h1 class="page-title"><?php
-						printf( __( 'Category Archives: %s', 'twentyeleven' ), '<span>' . single_cat_title( '', false ) . '</span>' );
-					?></h1>
 
 					<?php
 						$category_description = category_description();
